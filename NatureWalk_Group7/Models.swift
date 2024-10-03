a36633053b466e79d310d9bbc43fa6e6a898a33a
Nimratpreet Kaur <nimratkaur540@gmail.com>
2024-10-03 16:51:49 -0400

    Update Models.swift

--- NatureWalk_Group7/Models.swift
+++ NatureWalk_Group7/Models.swift
@@ -24,24 +24,7 @@ struct Session: Identifiable, Codable, Equatable {
     var imageName: String
     var address: String
 }
-struct Purchase: Identifiable, Codable {
-    var id: String // Assuming this is your id property
-    
-    // Other properties
-    var sessionId: String
-    var userId: String
-    var sessionName: String
-    var date: Date
-
-    // Initialize id explicitly in initializer
-    init(id: String = UUID().uuidString, sessionId: String, userId: String, sessionName: String, date: Date) {
-        self.id = id
-        self.sessionId = sessionId
-        self.userId = userId
-        self.sessionName = sessionName
-        self.date = date
-    }
-}       
+   
 
 
 
